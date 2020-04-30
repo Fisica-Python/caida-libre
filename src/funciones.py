59dfc8b16ef7874861cb2920c93189661d7a9325
Santiago Soto <Santiagosoto8@me.com>
2020-04-30 18:31:38 -0300

    Remuevo espacios

--- src/funciones.py
+++ src/funciones.py
@@ -1,13 +1,10 @@
 import math
 
-
 def pos_vertical(h_0, v_0, t):
     return h_0 + v_0 * t - 4.9 * (t ** 2)
 
-
 def tiempo_1(v, y):
     return (-v + math.sqrt(v ** 2 - 4 * (-4.9) * y)) / (-9.8)
-
 
 def tiempo_2(v, y):
     return (-v - math.sqrt(v ** 2 - 4 * (-4.9) * y)) / (-9.8)
